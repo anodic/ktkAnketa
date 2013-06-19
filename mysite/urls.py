044a60955bdc228c06675b6acf8be53da049ff5e
chip9munk <chip9munk@gmail.com>
2013-06-19 16:12:11 +0200

    dataset done 2

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import patterns, include, url
-
 from mysite.views import hello, current_datetime, hours_ahead
 
+
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^hello/$',hello),
 	url(r'^time/$',current_datetime),
 	url(r'^time/plus/(\d{1,2})/$',hours_ahead),
-
+	url(r'^admin/',include(admin.site.urls))
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
